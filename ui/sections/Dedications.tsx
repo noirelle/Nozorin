45c1e1a959caf93ca664d21840f125b03f0f36a4
Noirelle <xtln.tlnr@gmail.com>
2026-02-15 16:56:40 +0800

    feat: Implement guest authentication, user profile management, and initial UI structure.

--- ui/sections/Dedications.tsx
+++ ui/sections/Dedications.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 export default function Dedications() {
 
     const strangerNames = [
-        "Maecy", "Yvannah", "Chiela", "Freya", "Jenny", "Yana", "Earl", "JM", "Ashley", "Hannah"
+        "Maecy", "Yvannah", "Chiela", "Freya", "Jenny", "Yana", "Earl", "JM", "Ashley", "Hannah", "Diana, Princess of Wales"
     ];
 
     const themes = [
