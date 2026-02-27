b153f4b03bd3dad5fb8a2af203c171b30056cdbd
Noirelle <xtln.tlnr@gmail.com>
2026-02-27 13:21:05 +0800

    fix: build error in UI

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -100,6 +100,8 @@ export default function Home() {
 
             {incomingCall && (
                 <IncomingCallOverlay
+                    from_username={incomingCall.from_username}
+                    from_avatar={incomingCall.from_avatar}
                     from_country_name={incomingCall.from_country_name}
                     from_country={incomingCall.from_country}
                     mode={incomingCall.mode}
